3807aabd2225b73d2a84c336e3d1ae441b047f15
Andrew Owen <owenx148@umn.edu>
2015-09-29 15:20:12 -0500

    track time instead of distance in sample sets, let samplefactory handle speeds

--- src/main/java/org/opentripplanner/analyst/SampleSet.java
+++ src/main/java/org/opentripplanner/analyst/SampleSet.java
@@ -16,31 +16,30 @@ public class SampleSet {
     Vertex[] v1s;
 
     /* Distances to the vertices at the two ends of a road, one per sample. */
-    float[] d0s;
-    float[] d1s;
+    float[] t0s;
+    float[] t1s;
 
     public SampleSet (PointSet pset, SampleFactory sfac) {
         this.pset = pset;
         v0s = new Vertex[pset.capacity];
         v1s = new Vertex[pset.capacity];
-        d0s = new float[pset.capacity];
-        d1s = new float[pset.capacity];
+        t0s = new float[pset.capacity];
+        t1s = new float[pset.capacity];
         for (int i = 0; i < pset.capacity; i++) {
             Sample sample = sfac.getSample(pset.lons[i], pset.lats[i]);
             if (sample == null) {
-                d0s[i] = Float.NaN;
-                d1s[i] = Float.NaN;
+                t0s[i] = Float.NaN;
+                t1s[i] = Float.NaN;
                 continue;
             }
             v0s[i] = sample.v0;
             v1s[i] = sample.v1;
-            d0s[i] = sample.t0; // TODO time not distance in samples
-            d1s[i] = sample.t1; // TODO time not distance in samples
+            t0s[i] = sample.t0;
+            t1s[i] = sample.t1;
         }
     }
 
     public int[] eval (TimeSurface surf) {
-        final float WALK_SPEED = 1.3f;
         int[] ret = new int[pset.capacity];
         for (int i = 0; i < pset.capacity; i++) {
             int m0 = Integer.MAX_VALUE;
@@ -48,13 +47,13 @@ public class SampleSet {
             if (v0s[i] != null) {
                 int s0 = surf.getTime(v0s[i]);
                 if (s0 != TimeSurface.UNREACHABLE) {
-                    m0 = (int) (s0 + d0s[i] / WALK_SPEED);
+                    m0 = (int) (s0 + t0s[i]);
                 }
             }
             if (v1s[i] != null) {
                 int s1 = surf.getTime(v1s[i]);
                 if (s1 != TimeSurface.UNREACHABLE) {
-                    m1 = (int) (s1 + d1s[i] / WALK_SPEED);
+                    m1 = (int) (s1 + t1s[i]);
                 }
             }
             ret[i] = (m0 < m1) ? m0 : m1;
